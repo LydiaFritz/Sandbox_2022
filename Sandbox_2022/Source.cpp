f06236cdba17c65682f719e0f8421ecf18c2c07c
Lydia <Lydia.Fritz@gcu.edu>
2022-04-06 11:42:26 -0700

    some examples of try/catch blocks

--- Sandbox_2022/Source.cpp
+++ Sandbox_2022/Source.cpp
@@ -1,10 +1,39 @@
+#include <bitset>
 #include <iostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
 
-#include "License.h"
-#include "DriversLicense.h"
+int main()
+{
+    std::vector<double> vec;
+    vec.push_back(1);
+    vec.push_back(2);
+    try {
+        std::cout << vec.at(4) << std::endl;
+    }
+    catch (const std::exception& e) {
+        std::cout << e.what() << std::endl;
+    }
 
-int main() {
+    try {
+        std::bitset<4>{"0115"}; // Throws: only '0' or '1' expected
+    }
+    catch (std::invalid_argument const& ex) {
+        std::cout << "#1: " << ex.what() << '\n';
+    }
 
-	License* license = new DriversLicense();
-	return 0;
+    try {
+        int f = std::stoi("789"); // Throws: no conversion
+    }
+    catch (std::invalid_argument const& ex) {
+        std::cout << "#2: " << ex.what() << '\n';
+    }
+
+    try {
+        [[maybe_unused]] float f = std::stof("3.14"); // Throws: no conversion
+    }
+    catch (std::invalid_argument const& ex) {
+        std::cout << "#3: " << ex.what() << '\n';
+    }
 }
